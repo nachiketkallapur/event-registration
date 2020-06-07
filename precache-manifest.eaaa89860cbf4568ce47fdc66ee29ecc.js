e4080888778586841c454f55466c250e2dc61f58
nachiketkallapur <nachiketkallapur@gmail.com>
2020-06-07 22:17:54 +0530

    Updates

---
+++ precache-manifest.eaaa89860cbf4568ce47fdc66ee29ecc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51cf102d8985ce7953260b8e762e8135",
+    "url": "/event-registration/index.html"
+  },
+  {
+    "revision": "f0c04656444a1a17c910",
+    "url": "/event-registration/static/css/main.4034ffed.chunk.css"
+  },
+  {
+    "revision": "3fb2f8805bba726b244e",
+    "url": "/event-registration/static/js/2.5d2091ae.chunk.js"
+  },
+  {
+    "revision": "33087fb139e76863378abd724c9200fc",
+    "url": "/event-registration/static/js/2.5d2091ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0c04656444a1a17c910",
+    "url": "/event-registration/static/js/main.3336a0ac.chunk.js"
+  },
+  {
+    "revision": "7d2d2e597a2bf6d65d81",
+    "url": "/event-registration/static/js/runtime-main.a36f9562.js"
+  },
+  {
+    "revision": "2173754f0452c69b39ce43002f2abc03",
+    "url": "/event-registration/static/media/bg-mobile.2173754f.jpg"
+  },
+  {
+    "revision": "4a8d9265bcf7973d28672c4b2f827821",
+    "url": "/event-registration/static/media/bg.4a8d9265.jpg"
+  },
+  {
+    "revision": "59839c70b000fc6ded173f3131171b43",
+    "url": "/event-registration/static/media/logo.59839c70.svg"
+  },
+  {
+    "revision": "36dcff3dab8a6ffad89e89ba0c8cb9a8",
+    "url": "/event-registration/static/media/meditate.36dcff3d.png"
+  }
+]);
